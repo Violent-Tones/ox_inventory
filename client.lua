982187c13739162449a17676f51ee2992ead5d90
Bryan Gallardo <bryanjgallardo@gmail.com>
2023-11-30 19:12:26 -0800

    Add rob command

--- client.lua
+++ client.lua
@@ -737,6 +737,7 @@ local invHotkeys = false
 ---@type function?
 local function registerCommands()
 	RegisterCommand('steal', openNearbyInventory, false)
+	RegisterCommand('rob', openNearbyInventory, false)
 
 	local function openGlovebox(vehicle)
 		if not IsPedInAnyVehicle(playerPed, false) or not NetworkGetEntityIsNetworked(vehicle) then return end
