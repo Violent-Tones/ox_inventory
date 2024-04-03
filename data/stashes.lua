7e087e64fd647931ccaec5df2a6718bf568b35c0
Bryan Gallardo <bryanjgallardo@gmail.com>
2024-04-03 16:06:19 -0700

    :sparkles: Added stashes for mechanic businesses

--- data/stashes.lua
+++ data/stashes.lua
@@ -36,4 +36,109 @@ return {
 		weight = 70000,
 		groups = {['ambulance'] = 0}
 	},
+
+	{ -- LS Customs (City) Business Stash
+		coords = vector3(-345.07, -127.9, 38.33),
+		target = {
+			loc = vector3(-345.07, -127.9, 38.33),
+			length = 1.5,
+			width = 0.25,
+			heading = 340,
+			minZ = -1.5,
+			maxZ = 0.25,
+			label = 'Open Business Stash',
+			icon = 'fas fa-box-open',
+			drawSprite = true,
+		},
+		name = 'lscustoms',
+		label = 'LS Customs (City) Stash',
+		owner = false,
+		slots = 70,
+		weight = 120000,
+		groups = {['mechanic'] = 0}
+	},
+
+	{ -- LS Customs (Harmony) Business Stash
+		coords = vector3(1173.45, 2635.25, 37.0),
+		target = {
+			loc = vector3(1173.45, 2635.25, 37.0),
+			length = 0.25,
+			width = 1.5,
+			heading = 0,
+			minZ = -1,
+			maxZ = 1,
+			label = 'Open Business Stash',
+			icon = 'fas fa-box-open',
+			drawSprite = true,
+		},
+		name = 'lscustoms2',
+		label = 'LS Customs (Harmony) Stash',
+		owner = false,
+		slots = 70,
+		weight = 120000,
+		groups = {['mechanic2'] = 0}
+	},
+
+	{ -- LS Customs (Airport) Business Stash
+		coords = vector3(-1144.35, -2004.02, 13.35),
+		target = {
+			loc = vector3(-1144.32, -2004.04, 12.33),
+			length = 0.25,
+			width = 1.5,
+			heading = 315,
+			minZ = -1,
+			maxZ = 1,
+			label = 'Open Business Stash',
+			icon = 'fas fa-box-open',
+			drawSprite = true,
+		},
+		name = 'lscustoms3',
+		label = 'LS Customs (Airport) Stash',
+		owner = false,
+		slots = 70,
+		weight = 120000,
+		groups = {['mechanic3'] = 0}
+	},
+
+	{ -- Benny's Business Stash
+		coords = vec3(-199.58, -1314.65, 30),
+		target = {
+			loc = vector3(-196.44, -1314.42, 30.25),
+			length = 0.25,
+			width = 1.5,
+			heading = 0,
+			minZ = -1,
+			maxZ = 1,
+			label = 'Open Business Stash',
+			icon = 'fas fa-box-open',
+			drawSprite = true,
+		},
+		name = 'bennys',
+		label = 'Benny\'s Stash',
+		owner = false,
+		slots = 70,
+		weight = 120000,
+		groups = {['bennys'] = 0}
+	},
+
+	{ -- Beeker's Garage Business Stash
+		coords = vector3(108.22, 6631.22, 31),
+		target = {
+			loc = vector3(108.22, 6631.22, 31),
+			length = 0.25,
+			width = 1.5,
+			heading = 45,
+			minZ = -1,
+			maxZ = 1,
+			label = 'Open Business Stash',
+			icon = 'fas fa-box-open',
+			drawSprite = true,
+		},
+		name = 'beeker',
+		label = 'Beeker\'s Garage Stash',
+		owner = false,
+		slots = 70,
+		weight = 120000,
+		groups = {['beeker'] = 0}
+	},
 }
